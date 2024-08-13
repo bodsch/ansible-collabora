ef9b332f4d16851e8515facac4a899e40b2230fd
Bodo Schulz <bodo@boone-schulz.de>
2024-08-13 19:27:28 +0200

    change workflows

--- library/collabora_tls_certificates.py
+++ library/collabora_tls_certificates.py
@@ -31,8 +31,8 @@ class CollaboraTLSCertificates(object):
         self.group = module.params.get("group")
 
         self.ssl_cert = self.source.get("ssl_cert", None)
-        self.ssl_key  = self.source.get("ssl_key", None)
-        self.ssl_ca   = self.source.get("ssl_ca", None)
+        self.ssl_key = self.source.get("ssl_key", None)
+        self.ssl_ca = self.source.get("ssl_ca", None)
 
         self.ssl_files = []
         if self.ssl_cert:
